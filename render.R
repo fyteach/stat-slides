44a88e6a4b12ffe29686a29cac3dac93ee3a2478
fyemath <fyemath@users.noreply.github.com>
2026-02-10 13:55:52 -0500

    updated Topic 3

--- render.R
+++ render.R
@@ -1,6 +1,6 @@
-slides <- c("Topic-1-Statistical-Studies", "Topic-2-Graphical-Descriptions")
+slides <- c("Topic-1-Statistical-Studies", "Topic-2-Graphical-Descriptions", "Topic-3-Numercial-Descriptions")
 
-current_slide <- slides[2]
+current_slide <- slides[3]
 
 knitr::knit(paste0(current_slide, ".Rmd"), paste0(current_slide, ".md"))
 litedown::fuse(paste0(current_slide, ".md"), output = paste0(current_slide, ".html"))
